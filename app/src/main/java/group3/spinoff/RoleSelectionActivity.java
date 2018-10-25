fabbfc70e574b27f26e4dfa5897ea834d15e819f
Mads <Mads@stege.eu>
2018-10-25 16:45:19 +0200

    Changed UI for RoleSelectionActivity. Changed buttons look - removed grey border. Added new font and substituted in instead of bebasneueregular. Corrected strings.

--- app/src/main/java/group3/spinoff/RoleSelectionActivity.java
+++ app/src/main/java/group3/spinoff/RoleSelectionActivity.java
@@ -1,36 +1,42 @@
 package group3.spinoff;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class RoleSelectionActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button buttonCompanyLogin, buttonAppInfo, buttonEmployeeLogin;
 
+    private Button buttonAppInfo, buttonEmployeeLogin;
+    private TextView textViewCompanyLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_role_selection);
 
-
-        buttonCompanyLogin = findViewById(R.id.buttonCompanyLogIn);
+        textViewCompanyLogin = findViewById(R.id.textViewCompanyLogIn);
         buttonAppInfo = findViewById(R.id.buttonAppInfo);
         buttonEmployeeLogin = findViewById(R.id.buttonEmployeeLogIn);
+        TextView textViewCopyright = findViewById(R.id.textViewCopyright);
 
-        buttonCompanyLogin.setOnClickListener(this);
+        textViewCompanyLogin.setOnClickListener(this);
         buttonAppInfo.setOnClickListener(this);
         buttonEmployeeLogin.setOnClickListener(this);
+
+        textViewCompanyLogin.setPaintFlags(textViewCompanyLogin.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        textViewCopyright.setPaintFlags(textViewCopyright.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
 
     }
 
     @Override
     public void onClick(View view) {
-        if (view == buttonCompanyLogin) {
+        if (view == textViewCompanyLogin) {
             Intent i = new Intent(this, CompanyLoginActivity.class);
             startActivity(i);
         } else if (view == buttonAppInfo) {
