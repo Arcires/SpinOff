b14c90fdd8eb1d8c039a1e94ef16a5e2826c807d
peterxp@hotmail.fr <9875321elhabr>
2018-11-16 14:47:32 +0100

    Important Jacob code refactor

--- app/src/main/java/group3/spinoff/employeeUI/FeedbackHomeFragment.java
+++ app/src/main/java/group3/spinoff/employeeUI/FeedbackHomeFragment.java
@@ -1,7 +1,5 @@
 package group3.spinoff.employeeUI;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -13,16 +11,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 import group3.spinoff.R;
 
-class LandeOgByerData {
+class FeedbackData {
     List<String> meetings = Arrays.asList("Spinoff", "Company 1", "Company 9", "DTU", "Company 4",
             "DTU", "DTU", "DTU");
 
@@ -30,8 +25,6 @@ class LandeOgByerData {
             "Workplace meeting", "Machine Learning course", "Simple meeting", "Big Data course",
             "Advanced Mobile Application course", "Data Security course");
 
-    List<List<String>> byer = Arrays.asList(
-            Arrays.asList("null"));
 }
 
 
@@ -40,9 +33,7 @@ class LandeOgByerData {
  */
 public class FeedbackHomeFragment extends Fragment {
 
-    LandeOgByerData data = new LandeOgByerData();
-
-    HashSet<Integer> åbneLande = new HashSet<>(); // hvilke lande der lige nu er åbne
+    FeedbackData data = new FeedbackData();
 
     RecyclerView recyclerView;
 
@@ -55,25 +46,16 @@ public class FeedbackHomeFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
         recyclerView.setAdapter(adapter);
 
-        //getActivity().setContentView(recyclerView);
-
-        if (savedInstanceState!=null) {
-            åbneLande = (HashSet<Integer>) savedInstanceState.getSerializable("åbneLande");
-            recyclerView.getLayoutManager().onRestoreInstanceState(savedInstanceState.getParcelable("liste"));
-        }
-
         return recyclerView;
     }
 
 
     @Override
-    public void onSaveInstanceState(Bundle outState) { // Understøttelse for skærmvending - kan evt udelades
+    public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putSerializable("åbneLande", åbneLande);
-        outState.putParcelable("liste", recyclerView.getLayoutManager().onSaveInstanceState());
     }
 
-    RecyclerView.Adapter adapter = new RecyclerView.Adapter<EkspanderbartListeelemViewholder>() {
+    RecyclerView.Adapter adapter = new RecyclerView.Adapter<ListViewHolder>() {
 
         @Override
         public int getItemCount()  {
@@ -81,27 +63,27 @@ public class FeedbackHomeFragment extends Fragment {
         }
 
         @Override
-        public EkspanderbartListeelemViewholder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LinearLayout rodLayout = new LinearLayout(parent.getContext());
-            rodLayout.setOrientation(LinearLayout.VERTICAL);
-            EkspanderbartListeelemViewholder vh = new EkspanderbartListeelemViewholder(rodLayout);
-            vh.rodLayout = rodLayout;
-            vh.landeview = getLayoutInflater().inflate(R.layout.lekt04_listeelement, parent, false);
-            vh.overskrift = vh.landeview.findViewById(R.id.listeelem_overskrift);
-            vh.beskrivelse = vh.landeview.findViewById(R.id.listeelem_beskrivelse);
-            vh.åbnLukBillede = vh.landeview.findViewById(R.id.listeelem_billede);
-            vh.landeview.setOnClickListener(vh);
-            vh.landeview.setBackgroundResource(android.R.drawable.list_selector_background); // giv visuelt feedback når der trykkes på baggrunden
-            vh.åbnLukBillede.setOnClickListener(vh);
-//      vh.åbnLukBillede.setBackgroundResource(android.R.drawable.btn_default);
-            vh.rodLayout.addView(vh.landeview);
+        public ListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LinearLayout linearLayout = new LinearLayout(parent.getContext());
+            linearLayout.setOrientation(LinearLayout.VERTICAL);
+            ListViewHolder vh = new ListViewHolder(linearLayout);
+            vh.linearLayout = linearLayout;
+            vh.feedbackView = getLayoutInflater().inflate(R.layout.feedback_list_elements, parent, false);
+            vh.titleTextView = vh.feedbackView.findViewById(R.id.feedback_list_elements_title);
+            vh.descriptionTextView = vh.feedbackView.findViewById(R.id.feedback_list_elements_description);
+            vh.logoImageView = vh.feedbackView.findViewById(R.id.feedback_list_elements_image);
+            vh.feedbackView.setOnClickListener(vh);
+            vh.feedbackView.setBackgroundResource(android.R.drawable.list_selector_background);
+            vh.logoImageView.setOnClickListener(vh);
+//      vh.logoImageView.setBackgroundResource(android.R.drawable.btn_default);
+            vh.linearLayout.addView(vh.feedbackView);
             return vh;
         }
 
         @Override
-        public void onBindViewHolder(EkspanderbartListeelemViewholder vh, int position) {
-            vh.overskrift.setText(data.meetings.get(position));
-            vh.beskrivelse.setText(data.description.get(position));  // TEXT HERE
+        public void onBindViewHolder(ListViewHolder vh, int position) {
+            vh.titleTextView.setText(data.meetings.get(position));
+            vh.descriptionTextView.setText(data.description.get(position));  // TEXT HERE
         }
     };
 
@@ -111,15 +93,14 @@ public class FeedbackHomeFragment extends Fragment {
      * med findViewById() kun behøver at ske EN gang.
      * Se https://developer.android.com/training/material/lists-cards.html
      */
-    class EkspanderbartListeelemViewholder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        LinearLayout rodLayout;
-        TextView overskrift;
-        TextView beskrivelse;
-        ImageView åbnLukBillede;
-        View landeview;
-        ArrayList<TextView> underviews = new ArrayList<>();
+    class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        LinearLayout linearLayout;
+        TextView titleTextView;
+        TextView descriptionTextView;
+        ImageView logoImageView;
+        View feedbackView;
 
-        public EkspanderbartListeelemViewholder(View itemView) {
+        public ListViewHolder(View itemView) {
             super(itemView);
         }
 
@@ -127,18 +108,7 @@ public class FeedbackHomeFragment extends Fragment {
         public void onClick(View v) {
             final int position = getAdapterPosition();
 
-                int id = v.getId();
-                Toast.makeText(v.getContext(), "Klik på by nummer " + id + " i "+data.meetings.get(position), Toast.LENGTH_SHORT).show();
-
-
             getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameLayoutEmployee, new FeedbackView()).commit();
-
-            //android.app.Fragment feedback = new FeedbackView();
-            //FragmentManager fragmentManager=getActivity().getFragmentManager();
-            //FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-            //fragmentTransaction.replace(R.id.frameLayoutEmployee, feedback,"tag");
-            //fragmentTransaction.addToBackStack(null);
-            //fragmentTransaction.commit();
 
         }
     }
