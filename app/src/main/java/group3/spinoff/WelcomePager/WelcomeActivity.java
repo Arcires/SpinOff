474abe11df754257f71d0bcd6245eafbbb4f6733
Mads Stege <mads@stege.eu>
2018-11-22 23:13:04 +0100

    Fixed up WelcomeViewPager(WVP) logic. Corrected XML-layouts for WVP. Minor corrections in strings. Added Sliding dots to WVP. App now remembers if WVP have been shown before, and skips WVP if so.

--- app/src/main/java/group3/spinoff/WelcomePager/WelcomeActivity.java
+++ app/src/main/java/group3/spinoff/WelcomePager/WelcomeActivity.java
@@ -1,18 +1,22 @@
 package group3.spinoff.WelcomePager;
 
-import android.os.Build;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import group3.spinoff.R;
+import group3.spinoff.RoleSelectionActivity;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewPager viewPager;
     private int[] viewPagerLayouts = {R.layout.welcomepager_welcomescreen, R.layout.welcomepager_meetingscreen, R.layout.welcomepage_anonymousscreen, R.layout.welcomepage_feedbackscreen};
@@ -22,8 +26,19 @@ public class WelcomeActivity extends AppCompatActivity {
     private LinearLayout layoutDots;
     private ImageView[] imageViewdots;
 
+    private Button buttonNext, buttonSkip;
+
+    SharedPreferences sharedPrefs;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Boolean skipIntro = sharedPrefs.getBoolean("skipWelcomePager", false);
+        if (skipIntro) {
+            loadHome();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
@@ -32,23 +47,33 @@ public class WelcomeActivity extends AppCompatActivity {
         viewPagerAdapter = new ViewPagerAdapter(viewPagerLayouts, this);
         viewPager.setAdapter(viewPagerAdapter);
 
+        buttonNext = findViewById(R.id.buttonWelcomePagerNext);
+        buttonSkip = findViewById(R.id.buttonWelcomePagerSkip);
+
+        buttonNext.setOnClickListener(this);
+        buttonSkip.setOnClickListener(this);
+
         layoutDots = findViewById(R.id.dotsLayout);
         createDots(0);
+
+        editor = sharedPrefs.edit();
 
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
             }
 
             @Override
             public void onPageSelected(int position) {
                 createDots(position);
+                if (position == viewPagerLayouts.length - 1) {
+                    buttonNext.setText(R.string.welcomepager_button_finish);
+                    buttonSkip.setVisibility(View.INVISIBLE);
+                }
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
-
             }
         });
 
@@ -75,6 +100,33 @@ public class WelcomeActivity extends AppCompatActivity {
             }
 
         }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonNext) {
+            loadNextSlide();
+        } else if (view == buttonSkip) {
+            loadHome();
+            editor.putBoolean("skipWelcomePager", true).apply();
+        }
+    }
+
+    private void loadNextSlide() {
+        int nextSlide = viewPager.getCurrentItem() + 1;
+        if (nextSlide < viewPagerLayouts.length) {
+            viewPager.setCurrentItem(nextSlide);
+        } else {
+            loadHome();
+            editor.putBoolean("skipWelcomePager", true).apply();
+        }
+    }
+
+    private void loadHome() {
+        Intent i = new Intent(this, RoleSelectionActivity.class);
+        startActivity(i);
+        finish();
     }
 
 
